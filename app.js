6f2c48d54899a3a53185dbf122c06fce1d859072
RubinInsert <83633061+RubinInsert@users.noreply.github.com>
2024-10-04 16:49:29 +1000

    Front end + auth0 setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-const { auth } = require("express-openid-connect");
+const { auth, requiresAuth } = require("express-openid-connect");
+const { Console } = require("console");
 const app = express();
 const config = {
     authRequired: false,
@@ -8,18 +9,30 @@ const config = {
     secret: 'a long, randomly-generated string stored in env',
     baseURL: 'http://localhost:3000',
     clientID: 'zE2Xm70iilpjPHuw2jQ4mDduXE3JQbFA',
-    issuerBaseURL: 'https://dev-atqdicku.us.auth0.com'
+    issuerBaseURL: 'https://dev-atqdicku.us.auth0.com',
   };
   app.use(auth(config));
-  app.get('/*', (req, res) => {
-    console.log(path.join(__dirname, "/front", req.path));
-    if (req.oidc.isAuthenticated()) {
-        
-        res.sendFile(path.join(__dirname, "/front", req.path));
-    } else {
-        res.oidc.login({returnTo: "/home/index.html"});
-    }
+  app.get('/login', (req, res) => {
+    res.oidc.login({
+      returnTo: '/dashboard/index.html'
+    });
   });
+
+  app.get('/dashboard*', requiresAuth(), (req, res) => {
+    ServeFiles(req, res);
+    var pathDirs = req.path.split("/")
+    var isFileSpecified = pathDirs[pathDirs.length - 1].includes(".");
+    if (isFileSpecified) {
+      res.sendFile(path.join(__dirname, "/front/", req.url));
+    } else {
+      res.sendFile(path.join(__dirname, "/front/", req.url, "/index.html"));
+    }
+    console.log(path.join(__dirname, "/front/", req.url));
+  });
+  function ServeFiles(req, res) {
+    req.path
+  }
+
 //app.use(express.static('front'));
 // app.get("/login*", (req, res) => {
 //   console.log(req.url.endsWith("\\") + path.join(__dirname, "/front/", req.url));
